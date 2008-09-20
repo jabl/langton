2809bfc5d19acc4778c43f0e3a21657fe73e7312
Janne Blomqvist <blomqvist.janne@gmail.com>
2008-09-20 16:26:53 +0300

    Get rid of unused variables, switch to GPLv3.

--- langton.py
+++ langton.py
@@ -2,19 +2,18 @@
 
 #  Copyright (c) 2003-2004, 2008 Janne Blomqvist
 
-#  This program is free software; you can redistribute it and/or modify
+#  Langton is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
 
-#  This program is distributed in the hope that it will be useful,
+#  Langton is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 
 #  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#  along with Langton.  If not, see <http://www.gnu.org/licenses/>.
 
 __version__ = "Revision: 2.0 "
 
@@ -30,11 +29,6 @@ NORTH=0
 EAST=1
 SOUTH=2
 WEST=3
-
-FORWARD=0
-RIGHT=1
-BACK=3
-LEFT=4
 
 class Grid:
     "Grid for the ants to move on"
